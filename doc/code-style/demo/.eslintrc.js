4aa760b1f50b6be52ceb1819ad123f637025b5a5
九彩拼盘 <iamjoel007@gmail.com>
2020-01-19 12:01:26 +0800

    perf: add check rule

--- doc/code-style/demo/.eslintrc.js
+++ doc/code-style/demo/.eslintrc.js
@@ -18,19 +18,29 @@ module.exports = {
     // 提高代码质量
     "array-callback-return": "error", // 强制数组的某些需要返回值的回调函数中有 return 语句。
     "block-scoped-var": "error", // 强制把变量的使用限制在其定义的作用域范围内
-    "no-sequences": "error", // 不允许使用逗号操作符
-    "complexity": ["error", { "max": 10 }], // 程序中允许的最大环路复杂度
-    "consistent-return": "error", // 要求 return 语句要么总是指定返回的值，要么不指定
     "default-case": "error", // 要求 switch 语句中有 default 分支
     "no-empty-function": "error", // 禁止出现空函数
     "no-fallthrough": "error", // 禁止 case 贯穿。
     "no-labels": "error", // 禁用标签语句
     "no-redeclare": "error", // 禁止多次声明同一变量
     "no-return-assign": ["error", "always"], // 禁止在返回语句中赋值
+    "no-sequences": "error", // 不允许使用逗号操作符
+    "no-useless-concat": "warn", // 禁止不必要的字符串字面量或模板字面量的连接,
+    "no-eval": "error", // 禁用 eval()
+    "no-unused-vars": "warn", // 禁止出现未使用过的变量
+    "no-use-before-define": "error", // 禁止在变量定义之前使用它们
+    "complexity": ["error", { "max": 10 }], // 程序中允许的最大环路复杂度
+    "consistent-return": "error", // 要求 return 语句要么总是指定返回的值，要么不指定
     "max-depth": ["error", 5], // 强制块语句的最大可嵌套深度
     "max-lines-per-function": ["warn", 50], // 强制函数最大行数
     "max-params": ["warn", 4], // 限制函数定义中最大参数个数
+    "require-await": "error", // 禁止使用不带 await 表达式的 async 函数
+    
     // es6
     "no-var": "warn", // 要求使用 let 或 const 而不是 var
+    "no-duplicate-imports": "warn", // 禁止重复模块导入
+    "prefer-const": "warn", // 要求使用 const 声明那些声明后不再被修改的变量
+    "prefer-destructuring": "warn", // 优先使用数组和对象解构
+    "prefer-template": "warn" // 要求使用模板字面量而非字符串连接
   }
 }
